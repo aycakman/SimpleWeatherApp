a923d173f4550beb4191a63dd5026956812b6a6d
aycakman <aycaakman0@gmail.com>
2023-05-23 21:30:01 +0300

    get city names from JSON file for table view

--- SimpleWeatherApp/Controllers/CityViewController.swift
+++ SimpleWeatherApp/Controllers/CityViewController.swift
@@ -11,37 +11,45 @@ class CityViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     var cityManager = CityManager()
-    
+    var cityNames: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.register(UINib(nibName: "CityNameViewCell", bundle: nil), forCellReuseIdentifier: "CityNameViewCell")
         // Do any additional setup after loading the view.
-        if let localData = cityManager.readLocalFile(forName: "city.list") {
-            cityManager.parse(jsonData: localData)
-        }
+//        if let localData = cityManager.readLocalFile(forName: "city.list") {
+//            cityManager.parse(jsonData: localData)
+//        }
+        
+        cityNames = parseJson()
+        print(cityNames)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func parseJson() -> [String] {
+        var cityNames = [String]()
+        if let path = Bundle.main.path(forResource: "city.list", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let decoder = JSONDecoder()
+                let cities = try decoder.decode([City].self, from: data)
+                cityNames = cities.map { $0.name }
+                
+            } catch {
+                print("fetch the name in json file: \(error)")
+            }
+        }
+        return cityNames
     }
-    */
-
 }
 
 extension CityViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return cityNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CityNameViewCell", for: indexPath) as! CityNameViewCell
+        cell.cityNameLabel.text = cityNames[indexPath.row]
         return cell
     }
     
--- SimpleWeatherApp/Controllers/WeatherViewController.swift
+++ SimpleWeatherApp/Controllers/WeatherViewController.swift
@@ -17,19 +17,8 @@ class WeatherViewController: UIViewController {
         // Do any additional setup after loading the view.
         tableView.register(UINib(nibName: "WeatherViewCell", bundle: nil), forCellReuseIdentifier: "WeatherViewCell")
         tableView.register(UINib(nibName: "BigWeatherViewCell", bundle: nil), forCellReuseIdentifier: "BigWeatherViewCell")
-        
+       
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
